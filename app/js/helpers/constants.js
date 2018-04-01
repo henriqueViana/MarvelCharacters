837a36d56be21c0199b79e204ba133b2d1771baf
henriqueviana <henriquehvisa@gmail.com>
2018-04-01 06:01:39 -0300

    Create constants file

---
+++ app/js/helpers/constants.js
@@ -0,0 +1,5 @@
+export const URL = 'https://gateway.marvel.com:443/v1/public';
+export const APIKEY = '375e3771adbdf053196915a8a9c1fc5d';
+export const HASH = '1d11dc5ef9b57327501d6ccea0f9c3c4';
+export const TIMESTAMP = '1';
+export const LIMIT = '10';
